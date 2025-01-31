3ebdfb557c74dd22dcc330c939e370ffa6f0efdc
Alan Moreno <amoreno@perceptivesensors.com>
2025-01-31 13:54:56 -0700

    work experience elements

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,16 +8,32 @@ import { textVariant } from '../utils/motion';
 
 const ExperienceCard = ({ experience }) => {
   return (
-    <VerticalTimelineElement contentStyle={{ background: '1d1836', color: '#fff' }} 
-                              contentArrowStyle={{ borderRight: '7px solid #232631' }}
+    <VerticalTimelineElement contentStyle={{ border: '4px solid gold', background: '1d1836', color: '#fff' }} 
+                              contentArrowStyle={{ borderRight: '7px solid #232631' }}                              
                               date={experience.date}
                               iconStyle={{ background: experience.iconBg }}
-                              icon={<div>
+                              icon={<div className="flex justify-center items-center w-full h-full">
                                       <img src={experience.icon} 
                                           alt={experience.company_name}
                                           className="w-[60%] h-[60%] object-contain" />
                                   </div>}>
-        {experience.date}
+        <div>
+          <h3 className="text-white text-[24px] font-bold">
+            {experience.title}
+          </h3>
+          <p className="text-secondary text-[16px] font-semibold"
+             style={{ margin: 0 }}> 
+            {experience.company_name}
+          </p>
+        </div>
+        <ul className="mt-5 list-disc ml-5 space-y-2" >
+          {experience.points.map((point, index) => (
+            <li key={`experience-point-${index}`}
+                className="text-secondary text-[14px] pl-1 tracking-wider">
+              {point}
+            </li>
+          ))}
+        </ul>
     </VerticalTimelineElement>
   )
 }
