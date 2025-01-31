f71f26cd5e6789f73c96c4133b721bca74b6bb50
Alan Moreno <amoreno@perceptivesensors.com>
2025-01-31 12:04:46 -0700

    globe positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { GlobeCanvas } from './canvas';
 
+// replace mouse with arrow
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
@@ -22,18 +23,20 @@ const Hero = () => {
             This is my bio and I will write about me 
           </p>
         </div>
-        <GlobeCanvas />
+          <GlobeCanvas />
+        {/* <div className="w-[450px] h-[640px] mx-10">
+        </div> */}      
       </div>
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-end items-center -mx-6">
-        <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
+        <div className="w-[35px] h-[64px] rounded-3xl border-4 border-black flex justify-center items-start p-2">
           <motion.div 
-            animate={{ y: [0, 24, 0] }}
+            animate={{ y: [0, 14, 0] }}
             transition={{
               duration: 4,
               repeat: Infinity,
               repeatType: 'loop'
             }}
-            className="w-3 h-3 rounded-full bg-white mb-1"
+            className="w-3 h-3 rounded-full bg-black mb-1"
           />            
         </div>
       </div>
