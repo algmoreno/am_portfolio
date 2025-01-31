ba3fbc5f9876ac6c972ceb120541487ff6df4cf9
Alan Moreno <amoreno@perceptivesensors.com>
2025-01-31 16:07:21 -0700

    rough finish of tech balls; need to change shape

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -13,7 +13,15 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1,]} />
-        <meshStandardMaterial/>
+        <meshStandardMaterial color="#fff8eb"
+                              polygonOffset
+                              polygonOffsetFactor={-5}
+                              flatShading/>
+      <Decal 
+        position={[0, 0, 1]} 
+        rotation={[2 * Math.PI, 0, 6.25]}
+        flatShading
+        map={decal} />
       </mesh>
     </Float>
   )
@@ -26,9 +34,7 @@ const BallCanvas = ({ icon }) => {
             gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
       {/* <OrbitControls
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}/> */}
+        enableZoom={false} /> */}
         <Ball imgUrl={icon}/>
       </Suspense>
       <Preload all/>
