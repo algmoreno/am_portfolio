4c71953e9df2da0894136c5bc8f33eb13675c109
Alan Moreno <amoreno@perceptivesensors.com>
2025-02-04 11:26:55 -0700

    prelim works section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc'
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion'
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          My Work
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Projects.
+        </h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p 
+          variants={fadeIn("", "", .1, 1)}
+          className="mt-3 text-white text-[17px] max-w-3xl leading-[30px]"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Nesciunt, animi inventore doloremque voluptas consectetur eligendi, numquam esse reiciendis dolorem qui 
+          explicabo sed aliquam accusantium aut rerum, cupiditate sequi beatae aperiam?
+        </motion.p>
+      </div>
+    </>
   )
 }
 
